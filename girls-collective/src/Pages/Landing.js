0076322025105f5f61c7ee3728c3ea889d9adf03
Sishir Buddharaju <sishir@4thoughtstudios.com>
2021-01-08 11:17:53 -0500

    base

---
+++ girls-collective/src/Pages/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingBanner from "../Components/LandingPage/LandingBanner"
+
+function Landing() {
+    return (
+        <>
+            <LandingBanner/>
+        </>
+    )
+}
+
+export default Landing
